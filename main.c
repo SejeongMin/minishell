c626b592fb67b05b794449483c59a5e489b6ce32
엄성웅 <tjddnd3116@gmail.com>
2022-02-05 14:52:11 +0900

    fix main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: semin <semin@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 12:54:17 by soum              #+#    #+#             */
-/*   Updated: 2022/02/05 14:32:56 by soum             ###   ########.fr       */
+/*   Updated: 2022/02/05 14:50:23 by soum             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,10 +61,10 @@ int	main(int ac, char **av, char **envp)
 		{
 			parsing(data);
 			//all_print_list(data);
-			free_cmd_list(data);
 		}
-		free_env_list(data);
+		free_cmd_list(data);
 		//system("leaks minishell");
 	}
+	free_env_list(data);
 	return (0);
 }
